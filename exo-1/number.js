e546378b1e9fb76ed783afb9466f47c345efb6e3
Mamouro <sangaremoon@gmail.com>
2024-06-10 01:25:42 +0200

    TP1 Javascrip

--- exo-1/number.js
+++ exo-1/number.js
@@ -2,9 +2,21 @@ const nomb = 1.42;
 const phrase = "de toute évidence, je ne suis pas un nombre";
 
 
+const phe = "de toute évidence, je ne suis pas un nombr"
 
+en = parseInt(1.42);
 
+i = parseFloat(15)
 
+deci = parseFloat(1.42);
+
+parseInt(phe);
+
+parseFloat(phe);
+
+console.log(parseInt(phe));
+
+console.log(i); 
 
 
 
