5550bfbf48792c15eef9bc194eee708459fad378
MrPikuniku <75395240+MrPikuniku@users.noreply.github.com>
2024-06-13 18:13:11 +0200

    l'Exercice  4

---
+++ exo-4/chain.js
@@ -0,0 +1,43 @@
+
+
+
+const max = 10;
+const getRandomInt = () => Math.floor(Math.random() * max);
+function generateComments() {
+  const comments = [];
+  for (let i = 0; i < 10; i ++) {
+    comments.push({
+      title: `Comment ${i + 1}`,
+      user: i % 3 !== 0
+        ? { name: "John Doe", age: i % 2 === 0 ? (i % 10) * 10 : null }
+        : null
+    })
+  }
+  return comments;
+}
+
+for (let i = 0; i < 5; i++) {
+  const comment = generateComments()[getRandomInt()];
+  let text = comment.title + " - "
+  text += (comment.user?.name) ?? ("Anonyme")
+  
+  text += comment.user?.age!=null ? ` (${comment.user?.age})` : ""  
+  
+  console.log(text)
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
